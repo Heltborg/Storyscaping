4fc96b1f1b1e50ef1a4b2b3ca727fe2abd54e40c
CathKjeldahl <cath.kjeldahl@gmail.com>
2025-10-28 21:31:26 +0100

    Evt rettelse til plugin
    Kan slettes - hvis den roder i noget

--- astra-child-theme/bellis_plugin/index.php
+++ astra-child-theme/bellis_plugin/index.php
@@ -14,33 +14,31 @@
 }
 
 // Tilføj CSS
-function sdb_enqueue_styles() {
-    wp_enqueue_style('sdb-style', plugin_dir_url(__FILE__) . 'style.css');
+function bellis_enqueue_styles() {
+    wp_enqueue_style('bellis-style', plugin_dir_url(__FILE__) . 'style.css');
 }
-add_action('wp_enqueue_scripts', 'sdb_enqueue_styles');
+add_action('wp_enqueue_scripts', 'bellis_enqueue_styles');
 
 // Tilføj JS
-function sdb_enqueue_scripts() {
-    wp_enqueue_script('sdb-script', plugin_dir_url(__FILE__) . 'script.js', array('jquery'), null, true);
+function bellis_enqueue_scripts() {
+    wp_enqueue_script('bellis-script', plugin_dir_url(__FILE__) . 'script.js', array('jquery'), null, true);
 }
-add_action('wp_enqueue_scripts', 'sdb_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'bellis_enqueue_scripts');
 
-// Shortcode funktion
-function sdb_slide_box_form() {
-    $content = '';
-
-    $content .= '<div id="sdb-container">';
-    $content .= '<div class="sdb-box slide-top" id="sdb-box">';
-    $content .= '<div class="sdb-close-button" id="sdb-close">&#10006;</div>';
-    $content .= '<h2>FÆLLESSKABET STARTER HER</h2>';
-    $content .= '<img src="' . plugin_dir_url(__FILE__) . 'img/bellis_plugin_billede.png" alt="Bellis billede">';
-    $content .= '<p>Fællesskab starter med et hej og en kop kaffe. Hos Bellis mødes mennesker, idéer og grin i øjenhøjde. Klik dig ind, og mærk stemningen, hvor forbindelser opstår.</p>';
-    $content .= '<div class="button-holder">';
-    $content .= '<button id="sdb-button">Oplev Fællesskabet</button>';
-    $content .= '</div>'; // button-holder
-    $content .= '</div>'; // sdb-box
-    $content .= '</div>'; // sdb-container
+// Shortcode: [bellis_popup]
+function bellis_popup_box() {
+    $content  = '<div id="bellis-container">';
+    $content .= '  <div class="bellis-box slide-top" id="bellis-box">';
+    $content .= '      <div class="bellis-close-button" id="bellis-close">&#10006;</div>';
+    $content .= '      <h2>FÆLLESSKABET STARTER HER</h2>';
+    $content .= '      <img src="' . plugin_dir_url(__FILE__) . 'img/bellis_plugin_billede.png" alt="Bellis billede">';
+    $content .= '      <p>Fællesskab starter med et hej og en kop kaffe. Hos Bellis mødes mennesker, idéer og grin i øjenhøjde. Klik dig ind, og mærk stemningen, hvor forbindelser opstår.</p>';
+    $content .= '      <div class="button-holder">';
+    $content .= '          <button id="bellis-button">Oplev Fællesskabet</button>';
+    $content .= '      </div>';
+    $content .= '  </div>';
+    $content .= '</div>';
 
     return $content;
 }
-add_shortcode('slide_box', 'sdb_slide_box_form');
+add_shortcode('bellis_popup', 'bellis_popup_box');
--- astra-child-theme/bellis_plugin/script.js
+++ astra-child-theme/bellis_plugin/script.js
@@ -1,22 +1,21 @@
-jQuery(document).ready(function($){
-    var $box = $('.sdb-box');
+jQuery(document).ready(function($) {
+    var $box = $('#bellis-box');
 
     // Sørg for, at boksen starter skjult
     $box.addClass('slide-top');
 
-    // Vent 2 sekunder før boksen slides ned
-    setTimeout(function(){
+    // Vent 2 sekunder før den glider ned
+    setTimeout(function() {
         $box.removeClass('slide-top').addClass('slide-down');
     }, 2000);
 
-    // Luk boksen når krydset klikkes
-    $('#sdb-close').on('click', function(){
-        // Slide op igen
+    // Luk boksen
+    $('#bellis-close').on('click', function() {
         $box.removeClass('slide-down').addClass('slide-top');
     });
 
-    // Optional: knapklik
-    $('#sdb-button').on('click', function(){
+    // Knapfunktion
+    $('#bellis-button').on('click', function() {
         alert('Knap trykket!');
     });
 });
